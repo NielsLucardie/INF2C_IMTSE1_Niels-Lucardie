c3e6bf89dded7703780b419c2d9abfe637c7e14e
Niels Lucardie <s1062775@student.hsleiden.nl>
2012-04-12 09:41:06 +0200

    first commit test

---
+++ IPMEDT4FOTOBEWERKER/FotoBewerker.cpp
@@ -0,0 +1,18 @@
+/*
+ * FotoBewerker.cpp
+ *
+ *  Created on: Mar 5, 2012
+ *      Author: Niels
+ */
+
+#include "FotoBewerker.h"
+
+FotoBewerker::FotoBewerker() {
+	Screen* screen = new FotoScherm();
+	screen->show();
+}
+
+FotoBewerker::~FotoBewerker() {
+	// TODO Auto-generated destructor stub
+}
+
